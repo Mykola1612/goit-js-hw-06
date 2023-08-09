513e2fef35d6c07c443f0d36a8449aca33709a32
Rhdshfeyhccfu <130246853+Mykola1612@users.noreply.github.com>
2023-08-09 19:47:51 +0300

    Summary

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,2 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  if (email.value !== "" && password.value !== "") {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+    formEl.reset();
+  } else {
+    alert("Save all form fields");
+  }
+}
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const buttonEl = body.querySelector(".change-color");
+const colorEl = body.querySelector(".color");
+
+buttonEl.addEventListener("click", onRandomColor);
+
+function onRandomColor(event) {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = body.style.backgroundColor = `${getRandomHexColor()}`;
+}
